c4922006025c56147f71d111869371a08935997a
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-28 10:56:13 -0700

    Added starter pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
+import Container from "./components/Container"
 
 function App() {
-  return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  );
+  return <Container/>;
 }
 
 export default App;
