338f642e4b64adde07a1e7c9694aad62ec405275
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-29 08:38:24 -0700

    Added about content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function About() {
   return(
-    <div className="container">
-      div
+<div className="card m-3">
+  <div className="card-header">
+    <h2 className="display-4">My name is Kevin Hernandez</h2>
+  </div>
+  <div className="sub-card m-3">
+    <div className="card-body">
+      <h5 className="card-title">Web Developer</h5>
+      <p className="card-text">Studying at UC Berkeley Extension</p>
     </div>
+  </div>
+</div>
   );
 }
