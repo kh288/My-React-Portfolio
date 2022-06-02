2f2890d8f381422656708d91bf5dea817b32fac6
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-01 21:06:15 -0700

    functionality to contact started

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const emailCheck = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  const [errorEmail, setEmailError] = useState("");
+  const [errorsubject, seterrorsubject] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInput = (e) => {
+    const inputType = e.target.id;
+    const inputValue = e.target.value;
+
+    switch(inputType) {
+      case("email"):
+        setEmail(inputValue);
+        break;
+      case("subject"):
+        setSubject(inputValue);
+        break;
+      case("message"):
+        setMessage(inputValue);
+        break;
+      default:
+        break;
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!emailCheck.test(email)) {
+      setEmailError('Please enter a valid email');
+      
+    }
+  }
+
   return(
 <article className="card m-3">
   <div className="card-header">
@@ -10,11 +47,18 @@ export default function Contact() {
     <div className="card-body text-center">
       
       <div className="input-group mb-3">
-        <input type="text" className="form-control" placeholder="Email"></input>
+        <input
+          type="text"
+          value={email}
+          className="form-control"
+          placeholder="Email"
+        >
+        </input>
       </div>
       <div className="input-group mb-3">
         <input 
         type="text" 
+        value={subject}
         className="form-control" 
         placeholder="Subject">
         </input>
@@ -22,14 +66,18 @@ export default function Contact() {
 
       <div className="form-floating">
         <textarea 
-        className="form-control"
-        type="text" 
-          placeholder="Leave a comment here" 
-          id="floatingTextarea2">
+          className="form-control"
+          type="text" 
+          value={message}
+          placeholder="Your message here"
+        >
         </textarea>
       </div>
 
-      <button className="btn btn-primary m-3">
+      <button 
+        className="btn btn-primary m-3"
+        onClick={handleSubmit}
+      >
         Submit
       </button>
 
