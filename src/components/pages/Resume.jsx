f9359ffbfa9dd8e78e71f0ad0f2d6be20707b740
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-01 21:31:01 -0700

    Added Resume page contents

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,11 +1,76 @@
 import React from "react";
+import resume from "../../assets/Resume-2022-May.pdf"
 
 export default function Resume() {
   return(
-    <div>
-      <h2>
-        Resume
-      </h2>
+<article className="card m-3">
+  <div className="card-header">
+    <h2 className="display-4">Resume</h2>
+  </div>
+  <div className="card sub-card m-3">
+    <div className="card-body text-center">
+      <a className="btn btn-primary" rel="noreferrer" target="_blank" href={resume}>Resume PDF</a>
     </div>
+    <div className="card-body">
+      <div className="row gap-3 p-3">
+
+        <div className="col-md card p-3">
+          <h4 className="card-title">Frontend</h4>
+          <ul>
+            <li>
+              <p>HTML</p>
+            </li>
+            <li>
+              <p>CSS</p>
+            </li>
+            <li>
+              <p>Javascript</p>
+            </li>
+            <li>
+              <p>Handlebars</p>
+            </li>
+            <li>
+              <p>Bootstrap</p>
+            </li>
+            <li>
+              <p>jQuery</p>
+            </li>
+            <li>
+              <p>React</p>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md card p-3">
+          <h4 className="card-title">Backend</h4>
+          <ul>
+            <li>
+              <p>HTML</p>
+            </li>
+            <li>
+              <p>CSS</p>
+            </li>
+            <li>
+              <p>Javascript</p>
+            </li>
+            <li>
+              <p>Handlebars</p>
+            </li>
+            <li>
+              <p>Bootstrap</p>
+            </li>
+            <li>
+              <p>jQuery</p>
+            </li>
+            <li>
+              <p>React</p>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</article>
   );
 }
